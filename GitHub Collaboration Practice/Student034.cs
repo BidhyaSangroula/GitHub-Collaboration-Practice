dc6ab6bfdf1858f94564ccc1044419a7691f0a9e
Bidhya Sangroula <sangroba@mail.uc.edu>
2025-04-17 11:54:35 -0400

    Added A method

--- GitHub Collaboration Practice/Student034.cs
+++ GitHub Collaboration Practice/Student034.cs
@@ -9,4 +9,9 @@ namespace GitHub_Collaboration_Practice
     internal class Student034
     {
     }
+
+    public static void helloclass()
+        {
+            Console.WriteLine("Hello Eveybody");
+        }
 }
